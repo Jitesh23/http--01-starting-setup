4ab66dc039ed107ef6a23828921827a4287bbdf2
Jitesh23 <jiteshpowankar@gmail.com>
2018-12-01 04:35:25 +0530

    Added the funcionality to request the data and render on the UI

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,75 @@
+import React, {
+    Component
+} from 'react';
+
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state = {
+        posts: [],
+        selectedId : null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                }) 
+                this.setState({
+                    posts: updatedPosts
+                })
+                //console.log(response);
+            });
+    }
+
+    postSelectedHanlder = (id) => {
+        this.setState({selectedId: id})
+    }
+
+    render() {
+
+        const posts = this.state.posts.map(post => {
+            return <Post key = {
+                post.id
+            }
+            title = {
+                post.title
+            }
+            author = {
+                post.author
+            }
+
+            clicked = {
+                () => this.postSelectedHanlder(post.id)
+            }
+            />
+        })
+
+        return ( < div >
+            <section className = "Posts" >
+             { posts} 
+             </section>
+             <section >
+            < FullPost id = {this.state.selectedId} / >
+            </section> <
+            section >
+            <
+            NewPost / >
+            </section> 
+            </div>
+        );
+    }
+}
+
+export default Blog;
